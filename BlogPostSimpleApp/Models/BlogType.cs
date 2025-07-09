734f124a322a3df4efc78d0f888603ddcd0e34e4
Pragun786 <khannap3@mymacewan.ca>
2025-07-09 17:25:05 -0600

    Made some changes using Data Annotation

--- BlogPostSimpleApp/Models/BlogType.cs
+++ BlogPostSimpleApp/Models/BlogType.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogPostSimpleApp.Models
 {
+    [Table("BlogType")] 
     public class BlogType
     {
-        public int BlogTypeId { get; set; }    // Primary Key
+        [Key]
+        public int BlogTypeId { get; set; }
+
+        [Required] 
         public int Status { get; set; }
+
+        [Required] 
+        [MaxLength(20)] 
         public string Name { get; set; }
+
+        [MaxLength(400)] 
         public string Description { get; set; }
     }
 }
